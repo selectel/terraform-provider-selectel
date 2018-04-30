ac8bb28997ad47adc3773bf89720ed13ac5aaa1f
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-04-30 15:49:56 +0300

    Implement floating ip resource methods
    Add Create, Read, Delete methods and Importer function.

--- selvpc/resource_selvpc_floatingip_v2.go
+++ selvpc/resource_selvpc_floatingip_v2.go
@@ -1,9 +1,21 @@
 package selvpc
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/selectel/go-selvpcclient/selvpcclient/resell/v2/floatingips"
+)
 
 func resourceResellFloatingIPV2() *schema.Resource {
 	return &schema.Resource{
+		Create: resourceResellFloatingIPV2Create,
+		Read:   resourceResellFloatingIPV2Read,
+		Delete: resourceResellFloatingIPV2Delete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 		Schema: map[string]*schema.Schema{
 			"project_id": &schema.Schema{
 				Type:     schema.TypeString,
@@ -33,8 +45,6 @@ func resourceResellFloatingIPV2() *schema.Resource {
 			},
 			"servers": &schema.Schema{
 				Type:     schema.TypeList,
-				Optional: true,
-				ForceNew: false,
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -55,4 +65,76 @@ func resourceResellFloatingIPV2() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceResellFloatingIPV2Create(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	resellV2Client := config.resellV2Client()
+	ctx := context.Background()
+
+	projectID := d.Get("project_id").(string)
+	region := d.Get("region").(string)
+	opts := floatingips.FloatingIPOpts{
+		FloatingIPs: []floatingips.FloatingIPOpt{
+			{
+				Region:   region,
+				Quantity: 1,
+			},
+		},
+	}
+
+	floatingIPs, _, err := floatingips.Create(ctx, resellV2Client, projectID, opts)
+	if err != nil {
+		return err
+	}
+
+	if len(floatingIPs) != 1 {
+		return fmt.Errorf("can't get floating ip from the response")
+	}
+
+	d.SetId(floatingIPs[0].ID)
+
+	return resourceResellFloatingIPV2Read(d, meta)
+}
+
+func resourceResellFloatingIPV2Read(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	resellV2Client := config.resellV2Client()
+	ctx := context.Background()
+
+	floatingIP, _, err := floatingips.Get(ctx, resellV2Client, d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.Set("fixed_ip_address", floatingIP.FixedIPAddress)
+	d.Set("floating_ip_address", floatingIP.FloatingIPAddress)
+	d.Set("port_id", floatingIP.PortID)
+	d.Set("project_id", floatingIP.ProjectID)
+	d.Set("status", floatingIP.Status)
+	// Convert servers to a list of maps.
+	associatedServers := make([]map[string]interface{}, len(floatingIP.Servers))
+	for i, server := range floatingIP.Servers {
+		associatedServers[i] = map[string]interface{}{
+			"id":     server.ID,
+			"name":   server.Name,
+			"status": server.Status,
+		}
+	}
+	d.Set("servers", associatedServers)
+
+	return nil
+}
+
+func resourceResellFloatingIPV2Delete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	resellV2Client := config.resellV2Client()
+	ctx := context.Background()
+
+	_, err := floatingips.Delete(ctx, resellV2Client, d.Id())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
