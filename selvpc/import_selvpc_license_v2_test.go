ebcc4b7a53be2cb180f0178c64bf11c8921e5f63
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-05-03 21:33:02 +0300

    License V2: add import test (#26)
    Add TestAccResellV2LicenseImportBasic.

---
+++ selvpc/import_selvpc_license_v2_test.go
@@ -0,0 +1,29 @@
+package selvpc
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccResellV2LicenseImportBasic(t *testing.T) {
+	resourceName := "selvpc_license_v2.license_tf_acc_test_1"
+	projectName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccSelVPCPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResellV2LicenseDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResellV2LicenseBasic(projectName),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
