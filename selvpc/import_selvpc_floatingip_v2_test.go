133f74455249c9c6b2a527fb9caa44f909c337b5
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-05-03 20:55:47 +0300

    Floating IP V2: add import test (#25)
    Add TestAccResellV2FloatingIPImportBasic.

---
+++ selvpc/import_selvpc_floatingip_v2_test.go
@@ -0,0 +1,30 @@
+package selvpc
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccResellV2FloatingIPImportBasic(t *testing.T) {
+	resourceName := "selvpc_floatingip_v2.floatingip_tf_acc_test_1"
+	projectName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccSelVPCPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResellV2FloatingIPDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResellV2FloatingIPBasic(projectName),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				// ImportStateVerifyIgnore: []string{"public_key"},
+			},
+		},
+	})
+}
