5a3cd3089b9cb6124d070464b5f1bd693b9781e3
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-05-03 18:59:18 +0300

    FloatingIP V2: add acceptance test (#22)
    Add terraform acceptance test for the floating ip resource.

---
+++ selvpc/resource_selvpc_floatingip_v2_test.go
@@ -0,0 +1,97 @@
+package selvpc
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/selectel/go-selvpcclient/selvpcclient/resell/v2/floatingips"
+	"github.com/selectel/go-selvpcclient/selvpcclient/resell/v2/projects"
+)
+
+func TestAccResellV2FloatingIPBasic(t *testing.T) {
+	var floatingip floatingips.FloatingIP
+	var project projects.Project
+	projectName := acctest.RandomWithPrefix("tf-acc")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccSelVPCPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResellV2FloatingIPDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResellV2FloatingIPBasic(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResellV2ProjectExists("selvpc_project_v2.project_tf_acc_test_1", &project),
+					testAccCheckResellV2FloatingIPExists("selvpc_floatingip_v2.floatingip_tf_acc_test_1", &floatingip),
+					resource.TestCheckResourceAttr("selvpc_floatingip_v2.floatingip_tf_acc_test_1", "region", "ru-2"),
+					resource.TestCheckResourceAttr("selvpc_floatingip_v2.floatingip_tf_acc_test_1", "status", "DOWN"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckResellV2FloatingIPDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	resellV2Client := config.resellV2Client()
+	ctx := context.Background()
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "selvpc_floatingip_v2" {
+			continue
+		}
+
+		_, _, err := floatingips.Get(ctx, resellV2Client, rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("floatingip still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckResellV2FloatingIPExists(n string, floatingip *floatingips.FloatingIP) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		resellV2Client := config.resellV2Client()
+		ctx := context.Background()
+
+		foundFloatingIP, _, err := floatingips.Get(ctx, resellV2Client, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if foundFloatingIP.ID != rs.Primary.ID {
+			return fmt.Errorf("floatingip not found")
+		}
+
+		*floatingip = *foundFloatingIP
+
+		return nil
+	}
+}
+
+func testAccResellV2FloatingIPBasic(projectName string) string {
+	return fmt.Sprintf(`
+resource "selvpc_project_v2" "project_tf_acc_test_1" {
+  name = "%s"
+}
+
+resource "selvpc_floatingip_v2" "floatingip_tf_acc_test_1" {
+  project_id = "${selvpc_project_v2.project_tf_acc_test_1.id}"
+  region     = "ru-2"
+}`, projectName)
+}
