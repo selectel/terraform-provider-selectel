40323d6fcc971b54320766b13f1f5fb5fe622de7
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-09-26 14:13:18 +0300

    Properly deprecate the vpc_crossregion_subnet_v2
    Add resource with methods that return errors in its methods.
    
    Add documentation page without sidebar link.
    
    Add common helper that returns a valid error message.

--- selectel/regions.go
+++ selectel/regions.go
@@ -1,6 +1,9 @@
 package selectel
 
 import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -14,4 +17,10 @@ func expandVPCV2Regions(rawRegions *schema.Set) []string {
 	}
 
 	return expandedRegions
+}
+
+// hashRegions is a hash function to use with the "regions" set.
+func hashRegions(v interface{}) int {
+	m := v.(map[string]interface{})
+	return hashcode.String(fmt.Sprintf("%s-", m["region"].(string)))
 }
