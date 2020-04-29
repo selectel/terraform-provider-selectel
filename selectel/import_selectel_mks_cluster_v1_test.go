60ed482a23f2193218a14f4e0c5f1aa6d5f3db3e
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2020-04-29 15:52:07 +0300

    Provider: add test func to check import env
    Add "testAccCheckSelectelImportEnv" helper function.
    Update "TestAccMKSClusterV1ImportBasic".

--- selectel/import_selectel_mks_cluster_v1_test.go
+++ selectel/import_selectel_mks_cluster_v1_test.go
@@ -1,14 +1,10 @@
 package selectel
 
 import (
-	"errors"
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccMKSClusterV1ImportBasic(t *testing.T) {
@@ -24,32 +20,7 @@ func TestAccMKSClusterV1ImportBasic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccMKSClusterV1Basic(projectName, clusterName, kubeVersion),
-				Check: func(s *terraform.State) error {
-					rs, ok := s.RootModule().Resources[resourceName]
-					if !ok {
-						return fmt.Errorf("not found: %s", resourceName)
-					}
-					if rs.Primary.ID == "" {
-						return errors.New("no ID is set")
-					}
-
-					var projectID, region string
-					if v, ok := rs.Primary.Attributes["project_id"]; ok {
-						projectID = v
-					}
-					if v, ok := rs.Primary.Attributes["region"]; ok {
-						region = v
-					}
-
-					if err := os.Setenv("SEL_PROJECT_ID", projectID); err != nil {
-						return fmt.Errorf("error setting SEL_PROJECT_ID: %s", err)
-					}
-					if err := os.Setenv("SEL_REGION", region); err != nil {
-						return fmt.Errorf("error setting SEL_REGION: %s", err)
-					}
-
-					return nil
-				},
+				Check:  testAccCheckSelectelImportEnv(resourceName),
 			},
 			{
 				ResourceName:      resourceName,
--- selectel/provider_test.go
+++ selectel/provider_test.go
@@ -1,9 +1,12 @@
 package selectel
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -31,5 +34,34 @@ func TestProviderImpl(_ *testing.T) {
 func testAccSelectelPreCheck(t *testing.T) {
 	if v := os.Getenv("SEL_TOKEN"); v == "" {
 		t.Fatal("SEL_TOKEN must be set for acceptance tests")
+	}
+}
+
+func testAccCheckSelectelImportEnv(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceName)
+		}
+		if rs.Primary.ID == "" {
+			return errors.New("no ID is set")
+		}
+
+		var projectID, region string
+		if v, ok := rs.Primary.Attributes["project_id"]; ok {
+			projectID = v
+		}
+		if v, ok := rs.Primary.Attributes["region"]; ok {
+			region = v
+		}
+
+		if err := os.Setenv("SEL_PROJECT_ID", projectID); err != nil {
+			return fmt.Errorf("error setting SEL_PROJECT_ID: %s", err)
+		}
+		if err := os.Setenv("SEL_REGION", region); err != nil {
+			return fmt.Errorf("error setting SEL_REGION: %s", err)
+		}
+
+		return nil
 	}
 }
