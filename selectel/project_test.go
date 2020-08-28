fda00671b5b72397cfcabd8074047da7fe5bc827
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-28 16:40:19 +0300

    Add TestFlattenVPCProjectV2Theme unit test
    Add test for flattenVPCProjectV2Theme function.

---
+++ selectel/project_test.go
@@ -0,0 +1,50 @@
+package selectel
+
+import (
+	"testing"
+
+	"github.com/selectel/go-selvpcclient/selvpcclient/resell/v2/projects"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlattenVPCProjectV2Theme(t *testing.T) {
+	testCases := []struct {
+		have projects.Theme
+		want map[string]string
+	}{
+		{
+			have: projects.Theme{},
+			want: nil,
+		},
+		{
+			have: projects.Theme{
+				Color: "some_color",
+			},
+			want: map[string]string{
+				"color": "some_color",
+			},
+		},
+		{
+			have: projects.Theme{
+				Logo: "some_logo",
+			},
+			want: map[string]string{
+				"logo": "some_logo",
+			},
+		},
+		{
+			have: projects.Theme{
+				Color: "another_color",
+				Logo:  "another_logo",
+			},
+			want: map[string]string{
+				"color": "another_color",
+				"logo":  "another_logo",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		assert.Equal(t, testCase.want, flattenVPCProjectV2Theme(testCase.have))
+	}
+}
