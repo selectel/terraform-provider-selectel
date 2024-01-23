b41a52b2aaa30be3344e0dd9ad10a079cb06b56e
Dmitrii Chudinov <d.chudinov.spb@yandex.ru>
2024-01-23 22:05:38 +0300

    Fix test: import zone for dns v2.

--- selectel/import_selectel_domains_zone_v2_test.go
+++ selectel/import_selectel_domains_zone_v2_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDomainsZoneV2ImportBasic(t *testing.T) {
@@ -25,7 +26,17 @@ func TestAccDomainsZoneV2ImportBasic(t *testing.T) {
 				ResourceName:      fullResourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
+				ImportStateIdFunc: getTestZoneIDForImport,
 			},
 		},
 	})
+}
+
+func getTestZoneIDForImport(s *terraform.State) (string, error) {
+	resourceZoneFullName := "selectel_domains_zone_v2.zone_tf_acc_test_1"
+	resourceZone, ok := s.RootModule().Resources[resourceZoneFullName]
+	if !ok {
+		return "", fmt.Errorf("Not found zone: %s", resourceZoneFullName)
+	}
+	return resourceZone.Primary.Attributes["name"], nil
 }
