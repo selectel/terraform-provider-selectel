6f3d0cd3253165a00fc5a07de77ace059bcd2550
Dmitrii Chudinov <d.chudinov.spb@yandex.ru>
2024-02-06 23:14:29 +0300

    Fix acceptance tests for import domains_v2 resources

--- selectel/import_selectel_domains_zone_v2_test.go
+++ selectel/import_selectel_domains_zone_v2_test.go
@@ -2,6 +2,7 @@ package selectel
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -10,17 +11,16 @@ import (
 )
 
 func TestAccDomainsZoneV2ImportBasic(t *testing.T) {
-	projectName := acctest.RandomWithPrefix("tf-acc")
+	projectID := os.Getenv("SEL_PROJECT_ID")
 	fullResourceName := fmt.Sprintf("selectel_domains_zone_v2.%[1]s", resourceZoneName)
 	testZoneName := fmt.Sprintf("%s.xyz.", acctest.RandomWithPrefix("tf-acc"))
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccSelectelPreCheck(t) },
+		PreCheck:          func() { testAccSelectelPreCheckWithProjectID(t) },
 		ProviderFactories: testAccProviders,
-		CheckDestroy:      testAccCheckVPCV2ProjectDestroy,
+		CheckDestroy:      testAccCheckDomainsV2ZoneDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDomainsZoneV2Basic(projectName, resourceZoneName, testZoneName),
-				Check:  testAccCheckSelectelImportEnv(fullResourceName),
+				Config: testAccDomainsZoneV2WithoutProjectBasic(projectID, resourceZoneName, testZoneName),
 			},
 			{
 				ResourceName:      fullResourceName,
@@ -40,4 +40,12 @@ func getTestZoneIDForImport(s *terraform.State) (string, error) {
 	}
 
 	return resourceZone.Primary.Attributes["name"], nil
+}
+
+func testAccDomainsZoneV2WithoutProjectBasic(projectID, resourceName, zoneName string) string {
+	return fmt.Sprintf(`
+		resource "selectel_domains_zone_v2" %[2]q {
+			name = %[3]q
+			project_id = %[1]q
+		}`, projectID, resourceName, zoneName)
 }
