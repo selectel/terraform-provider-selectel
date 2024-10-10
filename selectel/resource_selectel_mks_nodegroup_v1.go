620ac14f8008744391001946b623b850e41e62cd
Artem Minasyan <minasian.a@selectel.ru>
2024-10-10 17:37:57 +0300

    [MKS-4439]Add checking local_volume with volume_type

--- selectel/resource_selectel_mks_nodegroup_v1.go
+++ selectel/resource_selectel_mks_nodegroup_v1.go
@@ -89,7 +89,6 @@ func resourceMKSNodegroupV1() *schema.Resource {
 			},
 			"volume_type": {
 				Type:          schema.TypeString,
-				ConflictsWith: []string{"local_volume"},
 				Optional:      true,
 				ForceNew:      true,
 			},
@@ -254,6 +253,10 @@ func resourceMKSNodegroupV1Create(ctx context.Context, d *schema.ResourceData, m
 		AvailabilityZone:          d.Get("availability_zone").(string),
 		UserData:                  d.Get("user_data").(string),
 		InstallNvidiaDevicePlugin: &installNvidiaDevicePlugin,
+	}
+
+	if createOpts.VolumeType != "" && !createOpts.LocalVolume {
+		return diag.FromErr(fmt.Errorf("can't use volume_type with local_volume: %w", err))
 	}
 
 	projectQuotas, _, err := quotas.GetProjectQuotas(selvpcClient, projectID, region)
