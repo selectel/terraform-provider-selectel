978a3a68b53c078b84dca1cff80a5532356f5a6c
sokolnikov.m <sokolnikov.m@selectel.com>
2025-09-04 09:02:09 +0300

    fix lint. delete unused function

--- selectel/craas_test.go
+++ selectel/craas_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	v1 "github.com/selectel/craas-go/pkg"
 	clientv1 "github.com/selectel/craas-go/pkg/v1/client"
-	clientv2 "github.com/selectel/craas-go/pkg/v2/client"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,33 +31,6 @@ func newCRaaSTestClient(rs *terraform.ResourceState, testAccProvider *schema.Pro
 	}
 
 	craasClient := v1.NewCRaaSClientV1(selvpcClient.GetXAuthToken(), craasEndpoint)
-
-	return craasClient, nil
-}
-
-func newCRaaSTestClientV2(rs *terraform.ResourceState, testAccProvider *schema.Provider) (*clientv2.ServiceClient, error) {
-	config := testAccProvider.Meta().(*Config)
-
-	var projectID string
-
-	if id, ok := rs.Primary.Attributes["project_id"]; ok {
-		projectID = id
-	}
-
-	selvpcClient, err := config.GetSelVPCClientWithProjectScope(projectID)
-	if err != nil {
-		return nil, fmt.Errorf("can't get selvpc client for craas V2 acc tests: %w", err)
-	}
-
-	craasEndpoint, err := getEndpointForCRaaS(selvpcClient, CRaaS)
-	if err != nil {
-		return nil, fmt.Errorf("can't get endpoint for craas V2 acc tests: %w", err)
-	}
-
-	craasClient, err := clientv2.NewCRaaSClientV2(selvpcClient.GetXAuthToken(), craasEndpoint)
-	if err != nil {
-		return nil, fmt.Errorf("can't get client for craas V2 acc tests: %w", err)
-	}
 
 	return craasClient, nil
 }
