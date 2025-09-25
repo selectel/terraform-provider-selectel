5753501ea9b5977270460ee9c4297fee1e9995b7
Вениамин Мурашов <45630710+MurashovVen@users.noreply.github.com>
2025-09-25 13:56:53 +0400

    Dedicated Servers: hotfix: rename dedicated servers to dedicated (#348)
    * fixes: rename dedicated servers to dedicated

---
+++ selectel/data_source_selectel_dedicated_os_v1.go
@@ -0,0 +1,206 @@
+package selectel
+
+import (
+	"context"
+	"log"
+	"slices"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/terraform-providers/terraform-provider-selectel/selectel/internal/api/dedicated"
+)
+
+func dataSourceDedicatedOSV1() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceDedicatedOSV1Read,
+		Schema: map[string]*schema.Schema{
+			"project_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"filter": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"configuration_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"location_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			// computed
+			"os": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"arch": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"os": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"scripts_allowed": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"ssh_key_allowed": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"partitioning": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceDedicatedOSV1Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	dsClient, diagErr := getDedicatedClient(d, meta)
+	if diagErr != nil {
+		return diagErr
+	}
+
+	filter := expandDedicatedOperatingSystemsSearchFilter(d)
+
+	log.Printf("[DEBUG] Getting %s '%#v'", objectOS, filter)
+
+	opSystems, _, err := dsClient.OperatingSystems(ctx, &dedicated.OperatingSystemsQuery{
+		LocationID: filter.locationID,
+		ServiceID:  filter.configurationID,
+	})
+	if err != nil {
+		return diag.FromErr(errGettingObjects(objectOS, err))
+	}
+
+	filteredOS := filterDedicatedOperatingSystems(opSystems, filter)
+
+	osFlatten := flattenDedicatedOperatingSystems(filteredOS)
+	if err := d.Set("os", osFlatten); err != nil {
+		return diag.FromErr(err)
+	}
+
+	ids := make([]string, 0, len(filteredOS))
+	for _, e := range filteredOS {
+		ids = append(ids, e.UUID)
+	}
+
+	slices.Sort(ids)
+
+	checksum, err := stringListChecksum(ids)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(checksum)
+
+	return nil
+}
+
+type dedicatedOperatingSystemsFilter struct {
+	name            string
+	version         string
+	configurationID string
+	locationID      string
+}
+
+func expandDedicatedOperatingSystemsSearchFilter(d *schema.ResourceData) dedicatedOperatingSystemsFilter {
+	filter := dedicatedOperatingSystemsFilter{}
+
+	filterSet, ok := d.Get("filter").(*schema.Set)
+	if !ok {
+		return filter
+	}
+
+	if filterSet.Len() == 0 {
+		return filter
+	}
+
+	resourceFilterMap := filterSet.List()[0].(map[string]interface{})
+
+	name, ok := resourceFilterMap["name"]
+	if ok {
+		filter.name = name.(string)
+	}
+
+	configurationID, ok := resourceFilterMap["configuration_id"]
+	if ok {
+		filter.configurationID = configurationID.(string)
+	}
+
+	locationID, ok := resourceFilterMap["location_id"]
+	if ok {
+		filter.locationID = locationID.(string)
+	}
+
+	version, ok := resourceFilterMap["version"]
+	if ok {
+		filter.version = version.(string)
+	}
+
+	return filter
+}
+
+func filterDedicatedOperatingSystems(list dedicated.OperatingSystems, filter dedicatedOperatingSystemsFilter) dedicated.OperatingSystems {
+	var filtered dedicated.OperatingSystems
+	for _, entry := range list {
+		if (filter.name == "" || entry.Name == filter.name) &&
+			(filter.version == "" || entry.VersionValue == filter.version) {
+			filtered = append(filtered, entry)
+		}
+	}
+
+	return filtered
+}
+
+func flattenDedicatedOperatingSystems(list dedicated.OperatingSystems) []interface{} {
+	res := make([]interface{}, len(list))
+	for i, e := range list {
+		sMap := make(map[string]interface{})
+		sMap["id"] = e.UUID
+		sMap["name"] = e.Name
+		sMap["arch"] = e.Arch
+		sMap["os"] = e.OSValue
+		sMap["version"] = e.VersionValue
+		sMap["scripts_allowed"] = e.ScriptAllowed
+		sMap["ssh_key_allowed"] = e.IsSSHKeyAllowed
+		sMap["partitioning"] = e.Partitioning
+
+		res[i] = sMap
+	}
+
+	return res
+}
---
+++ selectel/schema_selectel_dedicated_server_v1.go
@@ -0,0 +1,154 @@
+package selectel
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	dedicatedServerSchemaKeyProjectID                = "project_id"
+	dedicatedServerSchemaKeyConfigurationID          = "configuration_id"
+	dedicatedServerSchemaKeyLocationID               = "location_id"
+	dedicatedServerSchemaKeyOSID                     = "os_id"
+	dedicatedServerSchemaKeyPricePlanName            = "price_plan_name"
+	dedicatedServerSchemaKeyPublicSubnetID           = "public_subnet_id"
+	dedicatedServerSchemaKeyPrivateSubnet            = "private_subnet"
+	dedicatedServerSchemaKeyOSUserData               = "user_data"
+	dedicatedServerSchemaKeyOSHostName               = "os_host_name"
+	dedicatedServerSchemaKeyOSSSHKey                 = "ssh_key"
+	dedicatedServerSchemaKeyOSSSHKeyName             = "ssh_key_name"
+	dedicatedServerSchemaKeyOSPartitionsConfig       = "partitions_config"
+	dedicatedServerSchemaKeySoftRaidConfig           = "soft_raid_config"
+	dedicatedServerSchemaKeyDiskPartitions           = "disk_partitions"
+	dedicatedServerSchemaKeyName                     = "name"
+	dedicatedServerSchemaKeyLevel                    = "level"
+	dedicatedServerSchemaKeyDiskType                 = "disk_type"
+	dedicatedServerSchemaKeyMount                    = "mount"
+	dedicatedServerSchemaKeySize                     = "size"
+	dedicatedServerSchemaKeySizePercent              = "size_percent"
+	dedicatedServerSchemaKeyRaid                     = "raid"
+	dedicatedServerSchemaKeyFSType                   = "fs_type"
+	dedicatedServerSchemaKeyOSPassword               = "os_password"
+	dedicatedServerSchemaForceUpdateAdditionalParams = "force_update_additional_params"
+)
+
+func resourceDedicatedServerV1Schema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		// required params
+		dedicatedServerSchemaKeyProjectID: {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		dedicatedServerSchemaKeyConfigurationID: {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		dedicatedServerSchemaKeyLocationID: {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		dedicatedServerSchemaKeyOSID: {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		dedicatedServerSchemaKeyPricePlanName: {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+
+		// optional os params
+		dedicatedServerSchemaKeyOSPassword: {
+			Type:      schema.TypeString,
+			Sensitive: true,
+			Optional:  true,
+		},
+		dedicatedServerSchemaKeyOSUserData: {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		dedicatedServerSchemaKeyOSSSHKey: {
+			Type:      schema.TypeString,
+			Sensitive: true,
+			Optional:  true,
+		},
+		dedicatedServerSchemaKeyOSSSHKeyName: {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		dedicatedServerSchemaKeyOSPartitionsConfig: {
+			Type:     schema.TypeList,
+			Optional: true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					dedicatedServerSchemaKeySoftRaidConfig: {
+						Type:     schema.TypeList,
+						Optional: true,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								dedicatedServerSchemaKeyName: {
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								dedicatedServerSchemaKeyLevel: {
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								dedicatedServerSchemaKeyDiskType: {
+									Type:     schema.TypeString,
+									Required: true,
+								},
+							},
+						},
+					},
+					dedicatedServerSchemaKeyDiskPartitions: {
+						Type:     schema.TypeList,
+						Optional: true,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								dedicatedServerSchemaKeyMount: {
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								dedicatedServerSchemaKeySize: {
+									Type:     schema.TypeFloat,
+									Optional: true,
+								},
+								dedicatedServerSchemaKeySizePercent: {
+									Type:     schema.TypeFloat,
+									Optional: true,
+								},
+								dedicatedServerSchemaKeyRaid: {
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								dedicatedServerSchemaKeyFSType: {
+									Type:     schema.TypeString,
+									Optional: true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		// optional network params
+		dedicatedServerSchemaKeyPublicSubnetID: {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		dedicatedServerSchemaKeyPrivateSubnet: {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+
+		// optional misc
+		dedicatedServerSchemaKeyOSHostName: {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		dedicatedServerSchemaForceUpdateAdditionalParams: {
+			Type:     schema.TypeBool,
+			Optional: true,
+		},
+	}
+}
