1c13a237a19c920d34ed6754f14032bb389c7158
Sergey Golovkin <sergey@golovk.in>
2026-05-06 17:27:20 +0300

    [stage-2] Code review

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-selectel/selectel"
@@ -14,6 +12,5 @@ func main() {
 		ProviderFunc: func() *schema.Provider {
 			return selectel.Provider(version.Version)
 		},
-		Debug: os.Getenv("TF_DEBUG") == "1",
 	})
 }
